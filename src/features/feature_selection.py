4f49cdbc2a7cdba7413762e31f4c4e8516cb7d72
Abhishek <abhicoders@gmail.com>
2016-10-05 16:24:23 +0530

    Tab Indent

--- src/features/feature_selection.py
+++ src/features/feature_selection.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 __all__ = [
-            "TreeBasedSelection"
-            ]
+			"TreeBasedSelection"
+			]
 
 class TreeBasedSelection(object):
-    """
-    Trains a extra trees classifier and based on the number of features
-    to be selected chooses the top features based on the feature importance.
-    """
-    def __init__(self, estimator, target, n_features_to_select=None):
-        self.estimator            =  estimator
-        self.n_features_to_select =  n_features_to_select
-        self.target               =  target
+	"""
+	Trains a extra trees classifier and based on the number of features
+	to be selected chooses the top features based on the feature importance.
+	"""
+	def __init__(self, estimator, target, n_features_to_select=None):
+		self.estimator            =  estimator
+		self.n_features_to_select =  n_features_to_select
+		self.target               =  target
 
-    def fit(self, X, y=None):
-        self.estimator.fit(X, self.target)
+	def fit(self, X, y=None):
+		self.estimator.fit(X, self.target)
 
-        self.importances = self.estimator.feature_importances_
-        self.indices     = np.argsort(self.importances)[::-1]
+		self.importances = self.estimator.feature_importances_
+		self.indices     = np.argsort(self.importances)[::-1]
 
-        return self
+		return self
 
-    def transform(self, X):
-        return X[:, self.indices[:self.n_features_to_select]]
+	def transform(self, X):
+		return X[:, self.indices[:self.n_features_to_select]]
